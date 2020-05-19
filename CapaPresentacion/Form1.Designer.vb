6d461a8c219d1fccf9487006d3e346a154efb54d
Arman12VCh <64511325+Arman12VCh@users.noreply.github.com>
2020-05-18 23:38:41 -0500

    prueba
    comen

--- CapaPresentacion/Form1.Designer.vb
+++ CapaPresentacion/Form1.Designer.vb
@@ -24,14 +24,14 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.PanelMenuIzq = New System.Windows.Forms.Panel()
+        Me.btnBiblioteca = New System.Windows.Forms.Button()
+        Me.btnMatricula = New System.Windows.Forms.Button()
+        Me.btnHorarios = New System.Windows.Forms.Button()
+        Me.btnCursos = New System.Windows.Forms.Button()
+        Me.btnInicio = New System.Windows.Forms.Button()
         Me.PanelLogo = New System.Windows.Forms.Panel()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.btnInicio = New System.Windows.Forms.Button()
-        Me.btnCursos = New System.Windows.Forms.Button()
-        Me.btnHorarios = New System.Windows.Forms.Button()
-        Me.btnMatricula = New System.Windows.Forms.Button()
-        Me.btnBiblioteca = New System.Windows.Forms.Button()
         Me.PanelMenuIzq.SuspendLayout()
         Me.PanelLogo.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -51,6 +51,61 @@ Partial Class Form1
         Me.PanelMenuIzq.Name = "PanelMenuIzq"
         Me.PanelMenuIzq.Size = New System.Drawing.Size(811, 53)
         Me.PanelMenuIzq.TabIndex = 1
+        '
+        'btnBiblioteca
+        '
+        Me.btnBiblioteca.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnBiblioteca.Location = New System.Drawing.Point(704, 0)
+        Me.btnBiblioteca.Name = "btnBiblioteca"
+        Me.btnBiblioteca.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.btnBiblioteca.Size = New System.Drawing.Size(120, 53)
+        Me.btnBiblioteca.TabIndex = 6
+        Me.btnBiblioteca.Text = "Biblioteca"
+        Me.btnBiblioteca.UseVisualStyleBackColor = True
+        '
+        'btnMatricula
+        '
+        Me.btnMatricula.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnMatricula.Location = New System.Drawing.Point(584, 0)
+        Me.btnMatricula.Name = "btnMatricula"
+        Me.btnMatricula.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.btnMatricula.Size = New System.Drawing.Size(120, 53)
+        Me.btnMatricula.TabIndex = 5
+        Me.btnMatricula.Text = "Matrícula"
+        Me.btnMatricula.UseVisualStyleBackColor = True
+        '
+        'btnHorarios
+        '
+        Me.btnHorarios.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnHorarios.Location = New System.Drawing.Point(464, 0)
+        Me.btnHorarios.Name = "btnHorarios"
+        Me.btnHorarios.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.btnHorarios.Size = New System.Drawing.Size(120, 53)
+        Me.btnHorarios.TabIndex = 4
+        Me.btnHorarios.Text = "Horarios"
+        Me.btnHorarios.UseVisualStyleBackColor = True
+        '
+        'btnCursos
+        '
+        Me.btnCursos.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnCursos.Location = New System.Drawing.Point(344, 0)
+        Me.btnCursos.Name = "btnCursos"
+        Me.btnCursos.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.btnCursos.Size = New System.Drawing.Size(120, 53)
+        Me.btnCursos.TabIndex = 3
+        Me.btnCursos.Text = "Mis Cursos"
+        Me.btnCursos.UseVisualStyleBackColor = True
+        '
+        'btnInicio
+        '
+        Me.btnInicio.Dock = System.Windows.Forms.DockStyle.Left
+        Me.btnInicio.Location = New System.Drawing.Point(224, 0)
+        Me.btnInicio.Name = "btnInicio"
+        Me.btnInicio.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.btnInicio.Size = New System.Drawing.Size(120, 53)
+        Me.btnInicio.TabIndex = 2
+        Me.btnInicio.Text = "Inicio"
+        Me.btnInicio.UseVisualStyleBackColor = True
         '
         'PanelLogo
         '
@@ -80,61 +135,6 @@ Partial Class Form1
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(811, 50)
         Me.Panel1.TabIndex = 2
-        '
-        'btnInicio
-        '
-        Me.btnInicio.Dock = System.Windows.Forms.DockStyle.Left
-        Me.btnInicio.Location = New System.Drawing.Point(224, 0)
-        Me.btnInicio.Name = "btnInicio"
-        Me.btnInicio.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.btnInicio.Size = New System.Drawing.Size(120, 53)
-        Me.btnInicio.TabIndex = 2
-        Me.btnInicio.Text = "Inicio"
-        Me.btnInicio.UseVisualStyleBackColor = True
-        '
-        'btnCursos
-        '
-        Me.btnCursos.Dock = System.Windows.Forms.DockStyle.Left
-        Me.btnCursos.Location = New System.Drawing.Point(344, 0)
-        Me.btnCursos.Name = "btnCursos"
-        Me.btnCursos.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.btnCursos.Size = New System.Drawing.Size(120, 53)
-        Me.btnCursos.TabIndex = 3
-        Me.btnCursos.Text = "Mis Cursos"
-        Me.btnCursos.UseVisualStyleBackColor = True
-        '
-        'btnHorarios
-        '
-        Me.btnHorarios.Dock = System.Windows.Forms.DockStyle.Left
-        Me.btnHorarios.Location = New System.Drawing.Point(464, 0)
-        Me.btnHorarios.Name = "btnHorarios"
-        Me.btnHorarios.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.btnHorarios.Size = New System.Drawing.Size(120, 53)
-        Me.btnHorarios.TabIndex = 4
-        Me.btnHorarios.Text = "Horarios"
-        Me.btnHorarios.UseVisualStyleBackColor = True
-        '
-        'btnMatricula
-        '
-        Me.btnMatricula.Dock = System.Windows.Forms.DockStyle.Left
-        Me.btnMatricula.Location = New System.Drawing.Point(584, 0)
-        Me.btnMatricula.Name = "btnMatricula"
-        Me.btnMatricula.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.btnMatricula.Size = New System.Drawing.Size(120, 53)
-        Me.btnMatricula.TabIndex = 5
-        Me.btnMatricula.Text = "Matrícula"
-        Me.btnMatricula.UseVisualStyleBackColor = True
-        '
-        'btnBiblioteca
-        '
-        Me.btnBiblioteca.Dock = System.Windows.Forms.DockStyle.Left
-        Me.btnBiblioteca.Location = New System.Drawing.Point(704, 0)
-        Me.btnBiblioteca.Name = "btnBiblioteca"
-        Me.btnBiblioteca.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.btnBiblioteca.Size = New System.Drawing.Size(120, 53)
-        Me.btnBiblioteca.TabIndex = 6
-        Me.btnBiblioteca.Text = "Biblioteca"
-        Me.btnBiblioteca.UseVisualStyleBackColor = True
         '
         'Form1
         '
--- CapaPresentacion/Form1.vb
+++ CapaPresentacion/Form1.vb
@@ -1,2 +1,5 @@
 ﻿Public Class Form1
+    Private Sub btnInicio_Click(sender As Object, e As EventArgs) Handles btnInicio.Click
+        'nn
+    End Sub
 End Class
